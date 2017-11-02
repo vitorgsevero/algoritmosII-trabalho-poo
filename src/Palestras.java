3571ecc01b7474a14b95e743931064863dea955a
Vitor Severo <vitorgsevero@gmail.com>
2017-11-02 17:32:04 -0200

    Adicionando a opção de criar novas palestras caso o evento exista

--- src/Palestras.java
+++ src/Palestras.java
@@ -4,16 +4,63 @@ public class Palestras extends Evento {
 	protected String salaPalestra;
 	protected String horarioPalestra;
 	protected String resumoPalestra;
+	protected String tituloPalestra;
 	
 	
+	public void criaPalestra(){
+		
+		System.out.println("Informe o evento para vincular a nova palestra:");
+		this.pesquisaEvento = ler.nextLine();
+		
+		if (this.pesquisaEvento.equals(pesquisaEvento)){
+			
+			System.out.println("Informe o nome da nova palestra:");
+			this.tituloPalestra = ler.next();
+			
+		}
+		
+	}
 	
 	
-	
-	public String mostraResumo(){
+	public void mostraResumo(){
 		
 		
-		
-		return this.resumoPalestra;
+	
+	}
+
+	
+	
+	
+	public String getSalaPalestra() {
+		return salaPalestra;
+	}
+
+	public void setSalaPalestra(String salaPalestra) {
+		this.salaPalestra = salaPalestra;
+	}
+
+	public String getHorarioPalestra() {
+		return horarioPalestra;
+	}
+	
+	public void setHorarioPalestra(String horarioPalestra) {
+		this.horarioPalestra = horarioPalestra;
+	}
+
+	public String getResumoPalestra() {
+		return resumoPalestra;
+	}
+
+	public void setResumoPalestra(String resumoPalestra) {
+		this.resumoPalestra = resumoPalestra;
+	}
+	
+	public String getTituloPalestra() {
+		return tituloPalestra;
+	}
+
+	public void setTituloPalestra(String tituloPalestra) {
+		this.tituloPalestra = tituloPalestra;
 	}
 
 }
